d427e701aa612726ce3ed43b8d1eac285f99a341
Jian Yang <jianyang@JiandeMacBook-Pro.local>
2021-04-11 15:43:08 +0800

    Initial commit

---
+++ Example/YJDomainManager/YJViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJViewController.h
+//  YJDomainManager
+//
+//  Created by YJ on 04/11/2021.
+//  Copyright (c) 2021 YJ. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJViewController : UIViewController
+
+@end
